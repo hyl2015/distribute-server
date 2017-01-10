ba4693c35f1b33bad7f8ef036e8d1b2699257a9a
linlin.huang <linlin.huang@redatoms.com>
2017-01-10 16:19:10 +0800

    update database

--- sql/publish_server_data.sql
+++ sql/publish_server_data.sql
@@ -1,5 +1,3 @@
-
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -7,13 +5,15 @@ SET FOREIGN_KEY_CHECKS = 0;
 --  Records of `sys_menu`
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_menu` VALUES ('1', '0', '发布资源', '/resource', 'send');
+TRUNCATE TABLE `sys_menu`;
+INSERT INTO `sys_menu` VALUES ('1', '0', '发布资源', '/resources', 'send');
 COMMIT;
 
 -- ----------------------------
 --  Records of `sys_permission`
 -- ----------------------------
 BEGIN;
+TRUNCATE TABLE `sys_permission`;
 INSERT INTO `sys_permission` VALUES ('1', '系统管理员');
 COMMIT;
 
@@ -21,6 +21,7 @@ COMMIT;
 --  Records of `sys_permission_menu`
 -- ----------------------------
 BEGIN;
+TRUNCATE TABLE `sys_permission_menu`;
 INSERT INTO `sys_permission_menu` VALUES ('1', '1', '1');
 COMMIT;
 
@@ -28,6 +29,7 @@ COMMIT;
 --  Records of `user`
 -- ----------------------------
 BEGIN;
+TRUNCATE TABLE `user`;
 INSERT INTO `user` VALUES ('1', 'admin', 'admin', 'admin', 'admin', '1', '0', '0', '0');
 COMMIT;
 
