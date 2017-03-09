99b6bb3de2871d5abd10bc92b989a8fc67c7c401
linlin.huang <linlin.huang@redatoms.com>
2017-03-09 17:35:11 +0800

    调整数据库结构

--- server/sql/distribute_server.sql
+++ server/sql/distribute_server.sql
@@ -11,21 +11,40 @@
  Target Server Version : 50717
  File Encoding         : utf-8
 
- Date: 03/06/2017 15:14:37 PM
+ Date: 03/09/2017 17:34:15 PM
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+--  Table structure for `res_version`
+-- ----------------------------
+DROP TABLE IF EXISTS `res_version`;
+CREATE TABLE `res_version` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ver` varchar(255) NOT NULL COMMENT '版本名称',
+  `notes` varchar(1024) NOT NULL COMMENT '版本说明',
+  `git_ver` varchar(256) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '版本状态：0 创建，1 打包完成，2 上线，3 下线',
+  `pre_id` int(11) NOT NULL DEFAULT '0' COMMENT '基于某一个版本发布',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
 --  Table structure for `sys_config`
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_config`;
 CREATE TABLE `sys_config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `key` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
-  PRIMARY KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 --  Table structure for `sys_menu`
@@ -37,6 +56,8 @@ CREATE TABLE `sys_menu` (
   `menu_name` varchar(64) NOT NULL DEFAULT '' COMMENT '菜单名称',
   `menu_url` varchar(256) NOT NULL DEFAULT '' COMMENT '菜单链接',
   `menu_icon` varchar(256) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='用户菜单';
 
@@ -47,6 +68,8 @@ DROP TABLE IF EXISTS `sys_permission`;
 CREATE TABLE `sys_permission` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL DEFAULT '' COMMENT '权限名称',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='权限信息表';
 
@@ -73,8 +96,8 @@ CREATE TABLE `user` (
   `salt` char(6) DEFAULT NULL COMMENT '密码盐',
   `permission_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '权限ID',
   `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '删除状态：0:未删除,1:已经删除',
-  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_name_idx` (`user_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='用户表';
--- server/sql/distribute_server_data.sql
+++ server/sql/distribute_server_data.sql
@@ -1,45 +1,62 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : localhost
+ Source Database       : distribute_server
+
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : utf-8
+
+ Date: 03/09/2017 17:34:24 PM
+*/
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Records of `res_version`
+-- ----------------------------
+BEGIN;
+INSERT INTO `res_version` VALUES ('1', 'v1', 'ttt', '', '0', '0', '2017-03-09 14:13:25', '2017-03-09 10:56:25'), ('2', 'v2', 'ttt', '', '0', '1', '2017-03-09 10:56:31', '2017-03-09 10:56:31'), ('3', 'v1', 'test', 'develop_67cd0d004c1926af5d4aecddf6a40e00bf317391', '0', '0', '2017-03-09 15:15:57', '2017-03-09 15:15:57');
+COMMIT;
 
 -- ----------------------------
 --  Records of `sys_config`
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_config` VALUES ('config.app.dir', '/var/app'), ('config.resource.dir', '/var/res');
+INSERT INTO `sys_config` VALUES ('1', 'config.rn.dir', '/Users/linlin.huang/Documents/workspace/rn/lifeline', '2017-03-08 16:54:19', '2017-03-08 16:54:19'), ('2', 'config.resource.dir', '/tmp/res', '2017-03-08 16:54:19', '2017-03-08 16:54:19'), ('3', 'config.app.dir', '/tmp/app', '2017-03-08 16:54:19', '2017-03-08 16:54:19');
 COMMIT;
 
 -- ----------------------------
 --  Records of `sys_menu`
 -- ----------------------------
 BEGIN;
-TRUNCATE TABLE `sys_menu`;
-INSERT INTO `sys_menu` VALUES ('1', '0', '发布资源', '/resources', 'send');
-INSERT INTO `sys_menu` VALUES ('2', '0', '发布', '/publish', 'send');
+INSERT INTO `sys_menu` VALUES ('1', '0', '发布资源', '/resources', 'send', '2017-03-07 16:45:53', '2017-03-07 16:45:53'), ('2', '0', '发布', '/publish', 'send', '2017-03-07 16:45:53', '2017-03-07 16:45:53');
 COMMIT;
 
 -- ----------------------------
 --  Records of `sys_permission`
 -- ----------------------------
 BEGIN;
-TRUNCATE TABLE `sys_permission`;
-INSERT INTO `sys_permission` VALUES ('1', '系统管理员');
+INSERT INTO `sys_permission` VALUES ('1', '系统管理员', '2017-03-07 16:46:06', '2017-03-07 16:46:06');
 COMMIT;
 
 -- ----------------------------
 --  Records of `sys_permission_menu`
 -- ----------------------------
 BEGIN;
-TRUNCATE TABLE `sys_permission_menu`;
-INSERT INTO `sys_permission_menu` VALUES ('1', '1', '1');
-INSERT INTO `sys_permission_menu` VALUES ('2', '1', '2');
+INSERT INTO `sys_permission_menu` VALUES ('1', '1', '1'), ('2', '1', '2');
 COMMIT;
 
 -- ----------------------------
 --  Records of `user`
 -- ----------------------------
 BEGIN;
-TRUNCATE TABLE `user`;
-INSERT INTO `user` VALUES ('1', 'admin', 'admin', 'admin', 'admin', '1', '0', '0', '0');
+INSERT INTO `user` VALUES ('1', 'admin', 'admin', 'admin', 'admin', '1', '0', '2017-03-07 16:46:15', '2017-03-07 16:46:15');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
